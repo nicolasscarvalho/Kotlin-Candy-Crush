307ac46411581f6ee60cc5879a8fd7d090f97d79
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-03-10 10:04:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/candy_crush/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.candy_crush
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
